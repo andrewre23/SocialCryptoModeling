254e9bde2cd09a15e5be7c57304ffc1bf0774f7e
andrewre23 <andrewre@umich.edu>
2018-05-06 19:05:09 -0700

    added error handling for if subreddit has < n submissions

--- reddit/reddit_tools.py
+++ reddit/reddit_tools.py
@@ -28,7 +28,7 @@ class RedditCollector(object):
         with open('reddit/subreddits.txt', 'r') as f:
             for line in f:
                 subreddit = line.strip()
-                print('Extracting top {} comments for r/{}'.format(n, subreddit.capitalize()))
+                print('Extracting top {} submissions for r/{}'.format(n, subreddit.capitalize()))
                 sub = SubredditObject(subreddit=subreddit)
                 sub.write_top_submissions(n)
 
@@ -111,29 +111,33 @@ class SubredditObject(object):
             raise ValueError("n must be integer larger than 0")
         submissions = self.get_top_submissions(n)
         all_subs = {}
-        for num in range(n):
-            # iterate through all submissions and extract information to
-            # write to JSON object
-            sub = submissions[num]
-            # create dict to hold all data for submission
-            output = {}
-            # add submission-level data to output data
-            output['created'] = str(self.convert_from_utc(sub.created_utc))
-            output['title'] = sub.title
-            output['num_comments'] = sub.num_comments
-            output['upvote_ratio'] = sub.upvote_ratio
-            output['score'] = sub.score
-            output['num_crossposts'] = sub.num_crossposts
-            # create comment section and add all comments
-            sub.comments.replace_more(limit=n)
-            comments = [
-                (str(self.convert_from_utc(comment.created)),
-                 comment.body,
-                 comment.score)
-                for comment in sub.comments.list()
-            ]
-            output['comments'] = comments
-            # add to master list
-            all_subs[num + 1] = output
+        try:
+            for num in range(n):
+                # iterate through all submissions and extract information to
+                # write to JSON object
+                sub = submissions[num]
+                # create dict to hold all data for submission
+                output = {}
+                # add submission-level data to output data
+                output['created'] = str(self.convert_from_utc(sub.created_utc))
+                output['title'] = sub.title
+                output['num_comments'] = sub.num_comments
+                output['upvote_ratio'] = sub.upvote_ratio
+                output['score'] = sub.score
+                output['num_crossposts'] = sub.num_crossposts
+                # create comment section and add all comments
+                sub.comments.replace_more(limit=n)
+                comments = [
+                    (str(self.convert_from_utc(comment.created)),
+                     comment.body,
+                     comment.score)
+                    for comment in sub.comments.list()
+                ]
+                output['comments'] = comments
+                # add to master list
+                all_subs[num + 1] = output
+        except IndexError:
+            print('Max submissions reached')
+
         with open('reddit/submissions/{}.json'.format(self.subreddit.display_name.lower()), 'w') as outfile:
             json.dump(all_subs, outfile)
